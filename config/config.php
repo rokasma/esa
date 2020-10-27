bd511c041875d7449f34149588c137391c473a85
Rokas Martusevičius <rokas@expertmedia.lt>
2020-10-27 12:51:53 +0200

    Config comments

--- config/config.php
+++ config/config.php
@@ -1,9 +1,30 @@
 <?php
 
 return [
+    /**
+     * Add API key from https://smartsheet.com/
+     */
     'api_key' => '',
+
+
+
+    /**
+     * Enable this if you want to send emails too.
+     */
     'send_email' => false,
+
+
+
+    /**
+     * Enter reveiver.
+     */
     'email_to' => '',
+
+
+    
+    /**
+     * Enter additional receivers
+     */
     'additional_emails' => [
         // 'foo@bar.com'
     ]
